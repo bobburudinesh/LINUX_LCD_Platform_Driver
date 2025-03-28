d49db19f351ea88e9fce18a5f01ed9e78f125b14
Dinesh Bobburu <bobburudinesh123@gmail.com>
2025-03-27 19:09:59 -0500

    Implemented Device files creation using device attributes and refactored some code(moved structure models to header files).

--- lcd_platform_driver.c
+++ lcd_platform_driver.c
@@ -6,39 +6,98 @@
 #include <linux/gpio/consumer.h>
 #include "lcd_platform_driver.h"
 
-#undef pr_fmt
-#define pr_fmt(fmt) "%s : "fmt,__func__
-
-#define DEVICE_COUNT    1
-#define LCD_LINES (4+1+1+1)
-
-enum {LCD_RS,LCD_RW,LCD_EN,LCD_D4,LCD_D5,LCD_D6,LCD_D7};
-
-struct lcd_dev_private_data {
-	int lcd_scroll;
-	char lcdxy[8]; /*(16,16)*/
-	struct device *dev;
-	struct gpio_desc *desc[LCD_LINES];	
-};
-
-struct lcd_drv_private_data {
-	struct class *class_lcd;
-};
-
 
 int lcd_probe(struct platform_device *pdev);
 void lcd_remove(struct platform_device *pdev);
+int create_device_files(struct device *dev, struct lcd_dev_private_data *lcd_data);
+ssize_t lcdcmd_store (struct device *dev, struct device_attribute *attr, const char *buf, size_t count);
+ssize_t lcdtext_store (struct device *dev, struct device_attribute *attr, const char *buf, size_t count);
+ssize_t lcdscroll_store (struct device *dev, struct device_attribute *attr, const char *buf, size_t count);
+ssize_t lcdscroll_show (struct device *dev, struct device_attribute *attr, char *buf);
+ssize_t lcdxy_store (struct device *dev, struct device_attribute *attr, const char *buf, size_t count);
+ssize_t lcdxy_show (struct device *dev, struct device_attribute *attr, char *buf);
 
 
 struct lcd_dev_private_data lcd_data = {
-	.lcd_scroll = 0,
-	.lcdxy = "(1,1)"
+        .lcd_scroll = 0,
+        .lcdxy = "(1,1)"
 };
 struct lcd_drv_private_data drv_data;
 
+
+
+
+ssize_t lcdcmd_store (struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {
+	
+	return 0;	
+}
+DEVICE_ATTR_WO(lcdcmd);
+
+
+
+ssize_t lcdtext_store (struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {
+	return 0;
+}
+
+DEVICE_ATTR_WO(lcdtext);
+
+
+
+ssize_t lcdscroll_store (struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {
+	return 0;
+}
+
+ssize_t lcdscroll_show (struct device *dev, struct device_attribute *attr, char *buf) {
+	return 0;
+}
+
+DEVICE_ATTR_RW(lcdscroll);
+
+
+
+ssize_t lcdxy_store (struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {
+	return 0;
+}
+
+ssize_t lcdxy_show (struct device *dev, struct device_attribute *attr, char *buf) {
+	return 0;
+}
+
+DEVICE_ATTR_RW(lcdxy);
+
+struct attribute *lcd_attrs[] = {
+	&dev_attr_lcdcmd.attr,
+	&dev_attr_lcdtext.attr,
+	&dev_attr_lcdscroll.attr,
+	&dev_attr_lcdxy.attr,
+	NULL
+};
+
+struct attribute_group lcd_attr_group = {
+	.attrs = lcd_attrs
+};
+
+static const struct attribute_group *lcd_attr_groups[] = {
+	&lcd_attr_group,
+	NULL
+};
+
+int create_device_files(struct device *dev,struct lcd_dev_private_data *lcd_data) {
+	lcd_data->dev = device_create_with_groups(drv_data.class_lcd, dev, 0, lcd_data, lcd_attr_groups, "LCD16x2");
+ 	if(IS_ERR(lcd_data->dev)){
+		dev_err(dev, "Creation of device files failed\n");
+		return PTR_ERR(lcd_data->dev);
+	}	
+	return 0;
+}
+
+
 int lcd_probe(struct platform_device *pdev) {
+	int ret;
 	struct device *dev = &pdev->dev;
+	
 	dev_set_drvdata(dev, &lcd_data);
+	
 	lcd_data.desc[LCD_RS] = gpiod_get(dev, "rs", GPIOD_OUT_LOW);
 	lcd_data.desc[LCD_RW] = gpiod_get(dev, "rw", GPIOD_OUT_LOW);
 	lcd_data.desc[LCD_EN] = gpiod_get(dev, "en", GPIOD_OUT_LOW);
@@ -59,6 +118,13 @@ int lcd_probe(struct platform_device *pdev) {
 	/*Configure direction of GPIO'S and init LCD*/
 	
 	dev_info(dev,"LCD init successful\n");
+	ret = create_device_files(dev, &lcd_data);	
+	if(ret) {
+		dev_err(dev,"LCD sysfs device create failed\n");
+		return ret;
+	}
+
+	dev_info(dev,"probe sucessful\n");
 
 	return 0;
 }
--- lcd_platform_driver.h
+++ lcd_platform_driver.h
@@ -10,9 +10,21 @@
 #undef pr_fmt
 #define pr_fmt(fmt) "%s : "fmt,__func__
 #define DEVICE_COUNT    1
+#define LCD_LINES (4+1+1+1)
 
 
+enum {LCD_RS,LCD_RW,LCD_EN,LCD_D4,LCD_D5,LCD_D6,LCD_D7};
 
+struct lcd_dev_private_data {
+        int lcd_scroll;
+        char lcdxy[8]; /*(16,16)*/
+        struct device *dev;
+        struct gpio_desc *desc[LCD_LINES];
+};
+
+struct lcd_drv_private_data {
+        struct class *class_lcd;
+};
 
 
 
